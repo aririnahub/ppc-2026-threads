72b17f2a4748f23f828a7cd1f21e32ca24717ec8
aririnahub <asvlasova2005@gmail.com>
2026-05-16 21:33:59 +0300

    clang-tidy fix4

--- tasks/vlasova_a_simpson_method/stl/src/ops_stl.cpp
+++ tasks/vlasova_a_simpson_method/stl/src/ops_stl.cpp
@@ -7,6 +7,7 @@
 #include <utility>
 #include <vector>
 
+#include "util/include/util.hpp"
 #include "vlasova_a_simpson_method/common/include/common.hpp"
 
 namespace vlasova_a_simpson_method {
@@ -89,7 +90,7 @@ void VlasovaASimpsonMethodSTL::ComputePartialSumRange(int start_idx, int end_idx
   double local_sum = 0.0;
 
   for (int idx = start_idx; idx < end_idx; ++idx) {
-    size_t temp_idx = static_cast<size_t>(idx);
+    auto temp_idx = static_cast<size_t>(idx);
     for (size_t i = 0; i < dim; ++i) {
       cur_index[i] = static_cast<int>(temp_idx % static_cast<size_t>(dimensions_[i]));
       temp_idx /= static_cast<size_t>(dimensions_[i]);
@@ -104,7 +105,34 @@ void VlasovaASimpsonMethodSTL::ComputePartialSumRange(int start_idx, int end_idx
   partial_sum = local_sum;
 }
 
-bool VlasovaASimpsonMethodSTL::RunImpl() {
+double VlasovaASimpsonMethodSTL::RunSequential() const {
+  size_t dim = task_data_.a.size();
+  std::vector<int> cur_index(dim, 0);
+  std::vector<double> cur_point;
+  double sum = 0.0;
+
+  size_t total_points = 1;
+  for (size_t i = 0; i < dim; ++i) {
+    total_points *= static_cast<size_t>(dimensions_[i]);
+  }
+
+  for (size_t idx = 0; idx < total_points; ++idx) {
+    auto temp_idx = idx;
+    for (size_t i = 0; i < dim; ++i) {
+      cur_index[i] = static_cast<int>(temp_idx % static_cast<size_t>(dimensions_[i]));
+      temp_idx /= static_cast<size_t>(dimensions_[i]);
+    }
+
+    double weight = 0.0;
+    ComputeWeight(cur_index, weight);
+    ComputePoint(cur_index, cur_point);
+    sum += weight * task_data_.func(cur_point);
+  }
+
+  return sum;
+}
+
+double VlasovaASimpsonMethodSTL::RunParallel() const {
   size_t dim = task_data_.a.size();
 
   size_t total_points = 1;
@@ -112,36 +140,6 @@ bool VlasovaASimpsonMethodSTL::RunImpl() {
     total_points *= static_cast<size_t>(dimensions_[i]);
   }
 
-  // Для маленьких задач используем последовательное выполнение
-  if (total_points < 10000) {
-    std::vector<int> cur_index(dim, 0);
-    std::vector<double> cur_point;
-    double sum = 0.0;
-
-    for (size_t idx = 0; idx < total_points; ++idx) {
-      size_t temp_idx = idx;
-      for (size_t i = 0; i < dim; ++i) {
-        cur_index[i] = static_cast<int>(temp_idx % static_cast<size_t>(dimensions_[i]));
-        temp_idx /= static_cast<size_t>(dimensions_[i]);
-      }
-
-      double weight = 0.0;
-      ComputeWeight(cur_index, weight);
-      ComputePoint(cur_index, cur_point);
-      sum += weight * task_data_.func(cur_point);
-    }
-
-    double factor = 1.0;
-    for (size_t i = 0; i < dim; ++i) {
-      factor *= h_[i] / 3.0;
-    }
-
-    result_ = sum * factor;
-    GetOutput() = result_;
-    return true;
-  }
-
-  // Параллельное выполнение с std::thread (как у коллег)
   unsigned int num_threads = ppc::util::GetNumThreads();
   if (num_threads == 0) {
     num_threads = std::thread::hardware_concurrency();
@@ -150,7 +148,6 @@ bool VlasovaASimpsonMethodSTL::RunImpl() {
     num_threads = 2;
   }
 
-  // Не создаем больше потоков, чем нужно
   if (static_cast<size_t>(num_threads) > total_points) {
     num_threads = static_cast<unsigned int>(total_points);
   }
@@ -162,12 +159,12 @@ bool VlasovaASimpsonMethodSTL::RunImpl() {
   std::vector<double> partial_sums(num_threads, 0.0);
 
   size_t start_idx = 0;
-  for (unsigned int t = 0; t < num_threads; ++t) {
-    size_t end_idx = start_idx + points_per_thread + (t < remainder ? 1 : 0);
-    threads.emplace_back([this, start_idx, end_idx, &partial_sums, t]() {
+  for (unsigned int thread_idx = 0; thread_idx < num_threads; ++thread_idx) {
+    size_t end_idx = start_idx + points_per_thread + (thread_idx < remainder ? 1 : 0);
+    threads.emplace_back([this, start_idx, end_idx, &partial_sums, thread_idx]() {
       double local_sum = 0.0;
       ComputePartialSumRange(static_cast<int>(start_idx), static_cast<int>(end_idx), local_sum);
-      partial_sums[t] = local_sum;
+      partial_sums[thread_idx] = local_sum;
     });
     start_idx = end_idx;
   }
@@ -178,7 +175,23 @@ bool VlasovaASimpsonMethodSTL::RunImpl() {
     }
   }
 
-  double sum = std::accumulate(partial_sums.begin(), partial_sums.end(), 0.0);
+  return std::accumulate(partial_sums.begin(), partial_sums.end(), 0.0);
+}
+
+bool VlasovaASimpsonMethodSTL::RunImpl() {
+  size_t dim = task_data_.a.size();
+
+  size_t total_points = 1;
+  for (size_t i = 0; i < dim; ++i) {
+    total_points *= static_cast<size_t>(dimensions_[i]);
+  }
+
+  double sum;
+  if (total_points < 10000) {
+    sum = RunSequential();
+  } else {
+    sum = RunParallel();
+  }
 
   double factor = 1.0;
   for (size_t i = 0; i < dim; ++i) {
